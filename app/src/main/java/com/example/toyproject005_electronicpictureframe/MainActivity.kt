4299b329d1dd186ea9741a9e88dd321bd2c40cb6
kimjinsub <hikm9985@naver.com>
2022-01-08 21:02:14 +0900

    사진 가져오기

--- app/src/main/java/com/example/toyproject005_electronicpictureframe/MainActivity.kt
+++ app/src/main/java/com/example/toyproject005_electronicpictureframe/MainActivity.kt
@@ -1,21 +1,41 @@
 package com.example.toyproject005_electronicpictureframe
 
+
+import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 
+
+
+
+
 class MainActivity : AppCompatActivity() {
 
-    private val addPhotoButton : Button by lazy {
+    private val addPhotoButton: Button by lazy {
         findViewById<Button>(R.id.addphotoButton)
     }
 
-    private val startPhotoFrameButton : Button by lazy {
+    private val startPhotoFrameButton: Button by lazy {
         findViewById<Button>(R.id.startPhotoFrameButton)
     }
+
+    private val imageViewList: List<ImageView> by lazy {
+        mutableListOf<ImageView>().apply {
+            add(findViewById(R.id.imageView11))
+            add(findViewById(R.id.imageView12))
+            add(findViewById(R.id.imageView13))
+            add(findViewById(R.id.imageView21))
+            add(findViewById(R.id.imageView22))
+            add(findViewById(R.id.imageView23))
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,38 +44,75 @@ class MainActivity : AppCompatActivity() {
         initStartPhotoFrameButton()
     }
 
-    private  fun initAddPhotoButton(){
-        addPhotoButton.setOnClickListener{
-            when{
+    private fun initAddPhotoButton() {
+        addPhotoButton.setOnClickListener {
+            when {
                 ContextCompat.checkSelfPermission(
                     this,
                     android.Manifest.permission.READ_EXTERNAL_STORAGE
-                )== PackageManager.PERMISSION_GRANTED->{
+                ) == PackageManager.PERMISSION_GRANTED -> {
                     //TODO 권한이 잘 부여되었을 때 갤러리에서 사진을 선택하는 기능
+                    navigatePhotos()
                 }
-                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)->{
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
                     //TODO 교육용 팝업 호가인 후 권한 팝업을 띄우는 기능
                     showPermissonContextPopup()
                 }
-                else ->{
-                    requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+                else -> {
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                        1000
+                    )
                 }
             }
         }
     }
-    private  fun showPermissonContextPopup(){
+
+    private fun navigatePhotos(){
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, 2000)
+    }
+
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            1000 -> {
+                if(grantResults.isNotEmpty() && grantResults[0] ==PackageManager.PERMISSION_GRANTED){
+                    //TODO 권한이 부여된 것이다.
+                    navigatePhotos()
+                }else{
+                    Toast.makeText(this,"권한을 거부하셨습니다.",Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+
+            }
+        }
+    }
+
+    private fun showPermissonContextPopup() {
         AlertDialog.Builder(this)
             .setTitle("권한이 필요합니다.")
             .setMessage("전자액자 앱에서 사진을 불러오기 위해 권한이 필요합니다.")
-            .setPositiveButton("동의하기"){ _, _ ->
+            .setPositiveButton("동의하기") { _, _ ->
                 requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
             }
-            .setNegativeButton("취소하기",{ _,_-> })
+            .setNegativeButton("취소하기", { _, _ -> })
             .create()
             .show()
     }
 
-    private fun initStartPhotoFrameButton(){
+    private fun initStartPhotoFrameButton() {
 
     }
+
+
 }
